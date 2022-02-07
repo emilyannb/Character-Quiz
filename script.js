6864df3649bc76b4ecbd198983819a2b09b7edb2
emilyannb <97969416+emilyannb@users.noreply.github.com>
2022-02-07 10:48:17 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 	$(".results").click(function() {
 	let response = $(".firstInput").val();
 	let response2 = $(".secondInput").val();
-	$(".final").append("You chose to be " + response + " and you are wiling to sacrifice " + response2 + " people to survive.");
+	$(".final").append("You chose to be " + response + " a and you are wiling to sacrifice " + response2 + " people to survive.");
 });
 
 
